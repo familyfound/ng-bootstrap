6630e2b1abab3546a42abd7f781ca6c119cbb7cf
Jared Forsyth <jared@jaredforsyth.com>
2013-06-25 23:59:46 -0600

    initial

---
+++ Readme.md
@@ -0,0 +1,37 @@
+
+# ng-bootstrap
+
+  Angular bootstrapper - take a template, inject into the page, and bootstrap the app
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install familyfound/ng-bootstrap
+
+## API
+
+### bootstrap(template, parentDiv, app, placement)
+
+    template:
+     - either the string of html, or an element whose innerHTML will be used
+    parentDiv: selector OR element
+    app: string or list - passed to angular.boostrap
+    placement:
+     - int: index at which to insert
+     - "first": at the start
+     - "last": append
+     - el: before this element
+
+### bootstrap.append(template, parentDiv, app)
+
+placement = "last"
+
+### bootstrap.first(template, parentDiv, app)
+
+placement = "first"
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,54 @@
+
+var query = require('query')
+  , angular = require('angularjs');
+
+// template:
+//  - either the string of html, or an element whose innerHTML will be used
+// parentDiv: selector OR element
+// app: string or list - passed to angular.boostrap
+// placement:
+//  - int: index at which to insert
+//  - "first": at the start
+//  - "last": append
+//  - el: before this element
+function bootstrap(template, parentDiv, app, placement) {
+  var div = document.createElement('div')
+    , sel;
+  if (typeof (template) !== 'string' && template.innerHTML) {
+    template = template.innerHTML;
+  }
+  div.innerHTML = template;
+  div = div.firstElementChild;
+  if (typeof (parentDiv) === 'string') {
+    sel = parentDiv;
+    parentDiv = query(parentDiv);
+    if (!parentDiv) {
+      console.error('Failed to find node:', sel, 'aborting bootstrap');
+      return;
+    }
+  }
+  if (placement == 'first') placement = 0;
+  if (placement == 'last' || placement >= parentDiv.children.length) {
+    parentDiv.appendChild(div);
+  } else if (typeof(placement) === 'number') {
+    parentDiv.insertBefore(div, parentDiv.children[placement]);
+  } else {
+    parentDiv.insertBefore(div, placement);
+  }
+  if (typeof(app) == 'string') {
+    app = [app];
+  }
+  angular.bootstrap(div, app);
+  return div;
+}
+
+module.exports = bootstrap;
+
+module.exports.append = function (template, parentDiv, app) {
+  return bootstrap(template, parentDiv, app, 'last');
+};
+
+module.exports.first = function (template, parentDiv, app) {
+  return bootstrap(template, parentDiv, app, 0);
+};
+
